16f7b71f7a213c5ce6fd388dfa5ad705071045ae
DESKTOP-7JVSUCK\Frederik <frederikkuper@gmail.com>
2019-04-13 23:05:46 +0200

    added functions for getting daily top submissions and random submission

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const snoowrap = require("snoowrap");
 const Discord = require("discord.js");
 const auth = require("./auth.json");
 
-const client = new Discord.Client();
-
-const r = new snoowrap({
+const discordClient = new Discord.Client();
+const reddit = new snoowrap({
   userAgent: auth.redditApiToken.userAgent,
   clientId: auth.redditApiToken.clientId,
   clientSecret: auth.redditApiToken.clientSecret,
@@ -13,20 +12,44 @@ const r = new snoowrap({
 
 
 // Gets called after the Discord Bot comes online
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+discordClient.on('ready', () => {
+  console.log(`Logged in as ${discordClient.user.tag}!`);
 });
 
 // Gets called after a message is typed in the discord server
-client.on('message', msg => {
-  if (msg.content === '!haiku') {
-    r.getSubreddit('YoutubeHaiku')
-      .getRandomSubmission()
-      .then((submission) => {
-        msg.reply(submission.url);
-        msg.react('👌');
-      });
+discordClient.on('message', msg => {
+  switch (msg.content) {
+    case '!haiku':
+      getRandomSubmission('YoutubeHaiku')
+        .then((submission) => {
+          msg.reply(submission.url);
+          msg.react('👌');
+        });
+      break;
+    case '!haiku-top':
+      getRandomTopDailySubmission('YoutubeHaiku')
+        .then((topPost) => {
+          msg.reply(topPost.url);
+        });
+      break;
   }
 });
+
+function getRandomTopDailySubmission(subreddit) {
+  return reddit.getSubreddit(subreddit)
+    .getTop({time: 'day'})
+    .then((topPosts) => {
+      const postIndex = getRandomInt(topPosts.length);
+      return topPosts[postIndex];
+    });
+}
+
+function getRandomSubmission(subreddit) {
+  return reddit.getSubreddit(subreddit).getRandomSubmission();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
    
-client.login(auth.token);
+discordClient.login(auth.token);
