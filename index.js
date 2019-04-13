5f88da2fc2dbfc0753adeda76c9c148959c8e046
DESKTOP-7JVSUCK\Frederik <frederikkuper@gmail.com>
2019-04-13 15:29:50 +0200

    implemented example discord bot

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const snoowrap = require("snoowrap");
+const Discord = require("discord.js");
+const token = require("./auth.json").token;
+
+const client = new Discord.Client();
+
+// Gets called after the Discord Bot comes online
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Gets called after a message is typed in the discord server
+client.on('message', msg => {
+  if (msg.content === '!haiku') {
+    msg.reply('OMEGALUL');
+    msg.react('👌');
+  }
+});
+   
+client.login(token);
